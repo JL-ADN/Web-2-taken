9d5d1e915fefcab9936f72d5db09708a1f83e994
Adrien <adrienguimard8@gmail.com>
2022-01-27 16:42:41 +0100

    Print-lijst update niet klaar

---
+++ print-lijst/script.js
@@ -0,0 +1,7 @@
+var voetballer1 = { name: "Messi", eigenschap: "Dribble", club: "PSG", genre: "man" };
+
+var voetballer2 = { name: "Putellas", eigenschap: "precision", club: "FcBarcelona", genre: "girl" };
+
+var voetballer3 = { name: "Cantona", eigenschap: "powershot", club: "ManchesterUtd", genre: "man" };
+
+for (let object of Lijstvoetballer) { console.log(Lijstvoetballer); }
