191810559435b3e2705687789b5014dbef850a0d
Adrien <adrienguimard8@gmail.com>
2022-01-27 21:22:57 +0100

    Update print lijst 2

--- print-lijst/script.js
+++ print-lijst/script.js
@@ -4,4 +4,6 @@ var voetballer2 = { name: "Putellas", eigenschap: "precision", club: "FcBarcelon
 
 var voetballer3 = { name: "Cantona", eigenschap: "powershot", club: "ManchesterUtd", genre: "man" };
 
+const Lijstvoetballer = ["Messi", "Putellas", "Cantona"];
+
 for (let object of Lijstvoetballer) { console.log(Lijstvoetballer); }
