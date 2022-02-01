d83a5ec643bd98e34f7fdaeffebacdbc0a8c2e4f
Adrien <adrienguimard8@gmail.com>
2022-02-01 16:43:24 +0100

    Update Wie ben ik

---
+++ Wie-ben-ik/script.js
@@ -0,0 +1,13 @@
+'use strict'; // typ us en direct enter
+// typ log en direct enter
+console.log('Adrien oefening wekt');
+//h1 selecteren en de background color veranderen
+let h1Element = document.querySelector('.h1');
+console.log(h1Element);
+h1Element.style.backgroundColor = 'red';
+ 
+//UNDERLINE
+
+document.getElementById("Adrien").style.textDecoration = "underline"
+document.getElementById("Achraf").style.backgroundcolor = "underline"
+document.getElementById("George").style.backgroundcolor = "underline"
