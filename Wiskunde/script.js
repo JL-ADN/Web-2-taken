ca776338f01fd87880ae4f95420e801cb07d9320
Adrien <adrienguimard8@gmail.com>
2022-02-08 16:12:45 +0100

    LAst update wiskunde 2

--- Wiskunde/script.js
+++ Wiskunde/script.js
@@ -3,7 +3,8 @@
 // Som = variable.
 let getal1 = prompt("geef het eerste getal"); // Gaat een vraag stellen aan een de gebruiker voor de eerste getal.
 let getal2 = prompt(" geef de tweede getal"); // Gaat een vraag stellen aan een de gebruiker voor de tweede getal.
-console.log
+let som = (getal1*1) + (getal2*1);
+alert(som)
 //let som = (getal1 * 1) + (getal2 * 1); // De som van de getallen.
 // Getal1 en getal2 zijn stings geworden dus plaats je een *1.
 // Alert(som);
@@ -13,8 +14,8 @@ console.log
 let isGetal1KleinerDanGetal2 = getal1 < getal2
 
 if (isGetal1KleinerDanGetal2) {
-    alert('getal1 is groter dan getal2'); //Dit voeren we uit indien juist
+    alert('getal1 is kleiner dan getal2'); //Dit voeren we uit indien juist
 }
 else {
-    alert('getal1 is kleiner dan getal2'); // Dit voeren we uit indien fout
+    alert('getal1 is groter dan getal2'); // Dit voeren we uit indien fout
 }
