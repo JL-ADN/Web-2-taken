544f005e419945644c12ff7d78ba75133c327c57
Adrien <adrienguimard8@gmail.com>
2022-02-08 16:01:17 +0100

    Update Wiskunde taak

--- Wiskunde/script.js
+++ Wiskunde/script.js
@@ -1,9 +1,20 @@
-// alert gaat de pop up tonen - laten verschijnen met een bericht
-// het bericht hier zal 12 + 13 zijn
-// som = variable
-let getal1 = prompt("geef het eerste getal"); // gaat een vraag stellen aan een de gebruiker voor de eerste getal
-let getal2 = prompt(" geef de tweede getal"); // gaat een vraag stellen aan een de gebruiker voor de tweede getal
+// Alert gaat de pop up tonen - laten verschijnen met een bericht.
+// Het bericht hier zal 12 + 13 zijn.
+// Som = variable.
+let getal1 = prompt("geef het eerste getal"); // Gaat een vraag stellen aan een de gebruiker voor de eerste getal.
+let getal2 = prompt(" geef de tweede getal"); // Gaat een vraag stellen aan een de gebruiker voor de tweede getal.
 console.log
-let som = (getal1 * 1) + (getal2 * 1); // de som van de getallen
-// getal1 en getal2 zijn stings geworden dus plaats je een *1
-alert(som);
+//let som = (getal1 * 1) + (getal2 * 1); // De som van de getallen.
+// Getal1 en getal2 zijn stings geworden dus plaats je een *1.
+// Alert(som);
+
+// Getal1 en getal2 vergelijken.
+
+let isGetal1KleinerDanGetal2 = getal1 < getal2
+
+if (isGetal1KleinerDanGetal2) {
+    alert('getal1 is kleiner dan getal2'); //Dit voeren we uit indien juist
+}
+else {
+    alert('getal1 is groter dan getal2'); // Dit voeren we uit indien fout
+}
