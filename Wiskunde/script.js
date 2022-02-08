b6eccd8fc37cca23979b39998e0829849c69e6e7
Adrien <adrienguimard8@gmail.com>
2022-02-08 14:10:22 +0100

    Stap 1 Wiskunde taak

---
+++ Wiskunde/script.js
@@ -0,0 +1,9 @@
+// alert gaat de pop up tonen - laten verschijnen met een bericht
+// het bericht hier zal 12 + 13 zijn
+// som = variable
+let getal1 = prompt("geef het eerste getal"); // gaat een vraag stellen aan een de gebruiker voor de eerste getal
+let getal2 = prompt(" geef de tweede getal"); // gaat een vraag stellen aan een de gebruiker voor de tweede getal
+console.log
+let som = (getal1 * 1) + (getal2 * 1); // de som van de getallen
+// getal1 en getal2 zijn stings geworden dus plaats je een *1
+alert(som);
