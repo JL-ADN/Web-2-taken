e31258813af27b1e0ab5fbd88e3ccbe3cd596aa4
Adrien <adrienguimard8@gmail.com>
2022-02-10 09:35:14 +0100

    Update print-lijst 10/02/22

--- print-lijst/script.js
+++ print-lijst/script.js
@@ -1,5 +1,6 @@
 var voetballer1 = { name: "Messi", age: "34", club: "psg" };
 var voetballer2 = { name: "Putellas", age: "27", club: "fcbarcelona", };
 var voetballer3 = { name: "Cantona", age: "55", club: "manchester", };
-var lijstvoetballer = { voetballer1, voetballer2, voetballer3 }
-for (let voetballer of lijstobjecten) { console.log(lijstvoetballer); }
+var lijstvoetballer = [voetballer1, voetballer2, voetballer3]
+
+for (let voetballer of lijstvoetballer) { console.log("lijstvoetballer"); }
