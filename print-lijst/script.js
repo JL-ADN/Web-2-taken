688422b308130a74c83a942a2254147b30a0246f
Adrien <adrienguimard8@gmail.com>
2022-02-11 20:02:14 +0100

    .

--- print-lijst/script.js
+++ print-lijst/script.js
@@ -4,6 +4,6 @@ var voetballer2 = { name: "Putellas", eigenschap: "precision", club: "FcBarcelon
 
 var voetballer3 = { name: "Cantona", eigenschap: "powershot", club: "ManchesterUtd", genre: "man" };
 
-const Lijstvoetballer = ["Messi", "Putellas", "Cantona"];
+const Lijstvoetballer = ["Messi", "Putellas", "Cantona"]; 
 
 for (let object of Lijstvoetballer) { console.log(Lijstvoetballer); }
