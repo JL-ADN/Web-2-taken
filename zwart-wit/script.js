5502c2b3ac88f49dfa9e495195dcf0792ec0afa1
Adrien <adrienguimard8@gmail.com>
2022-02-17 11:01:59 +0100

    Zwart Wit deel1

--- zwart-wit/script.js
+++ zwart-wit/script.js
@@ -1,20 +1,23 @@
 const select = document.querySelector('select');
-  const html = document.querySelector('html');
-  document.body.style.padding = '10px';
+const html = document.querySelector('html');
+document.body.style.padding = '10px';
 
-  function updateBackgroundAndTextColor(bgColor, textColor) {
-    html.style.backgroundColor = bgColor;
-    html.style.color = textColor;
+function updateBackgroundAndTextColor(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+select.onchange = function () {
+  console.log('huidige waarde van de select / dropdown:', select.value);
+  if (select.value === 'black') {
+    updateBackgroundAndTextColor('black', 'white');
+
+  } else if (select.value === 'white') {
+    updateBackgroundAndTextColor('white', 'black');
   }
 
-  select.onchange = function () {
-    console.log('huidige waarde van de select / dropdown:', select.value);
-    if (select.value === 'black') {
-        updateBackgroundAndTextColor('black', 'white');
-      } else if (select.value === 'white') {
-        updateBackgroundAndTextColor('white', 'black');
-      }
-    // NIETS VERANDEREN AAN CODE HIERBOVEN
-    // voeg hier code toe, zodat afhankelijk van de waarde, de kleur word aangepast
-
-  }
+  // NIETS VERANDEREN AAN CODE HIERBOVEN
+  // voeg hier code toe, zodat afhankelijk van de waarde, de kleur word aangepast
+else if ( select.value === 'red') {updateBackgroundAndTextColor('red','white', 'black')}
+else if ( select.value === 'green') {updateBackgroundAndTextColor('green','black','red' , 'black')}
+}
