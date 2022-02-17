0e574f02529890345cf0aedb5ba599949c8fc1dc
Adrien <adrienguimard8@gmail.com>
2022-02-17 12:09:43 +0100

    Weather opdracht 17/02

--- weather/script.js
+++ weather/script.js
@@ -1,20 +1,33 @@
 const select = document.querySelector('select');
-  // const html = document.querySelector('.output');
-  const html = document.querySelector('body');
+// const html = document.querySelector('.output');
+const html = document.querySelector('body');
 
-  function updateBgTextColor(bgColor, textColor) {
-    html.style.backgroundColor = bgColor;
-    html.style.color = textColor;
-  }
+function updateBgTextColor(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
 
-  select.onchange = function () {
-    const choice = select.value;
+select.onchange = function () {
+  const choice = select.value;
+  console.log('gekozen optie', choice);
 
-    // ADD SWITCH STATEMENT
-    // HIERBOVEN NIETS VERANDEREN
-    // je code met switch case komt vanaf hier:
-    
-
+  // ADD SWITCH STATEMENT
+  // HIERBOVEN NIETS VERANDEREN
+  // je code met switch case komt vanaf hier:
+  switch (choice) {
+    case "sunny": 
+    updateBgTextColor('yellow', 'black');
+      break;
+    case "rainy":
+      updateBgTextColor("blue", 'white');
+      break;
+    case "snowing":
+      updateBgTextColor("white", "grey");
+      break;
+    case "overcast":
+      updateBgTextColor("grey", "black");
+      break;
     // tot hier
     // HIERONDER NIETS VERANDEREN
   }
+}
