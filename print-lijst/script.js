2ebf52e813a1505f8d05338a2b5bd8906b2c721f
Adrien <adrienguimard8@gmail.com>
2022-02-24 10:38:29 +0100

    Update Html netlify lijst 24/02

--- print-lijst/script.js
+++ print-lijst/script.js
@@ -4,3 +4,13 @@ var voetballer2 = { name: "Putellas", age: "27", club: "fcbarcelona", };
 var voetballer3 = { name: "Cantona", age: "55", club: "manchester", };
 var lijstvoetballer = [voetballer1, voetballer2, voetballer3]
 for (let voetballer of lijstvoetballer) { console.log("lijstvoetballer"); }
+
+
+
+const namen = ["Dirk", "Sandy", "Tormund", "Dahlia"];
+
+for (let i = 0; i < namen.length; i++) {
+    // Gebruik de teller i als index van de array
+    // Elke iteratie wordt er dus een nieuw element aangesproken
+    console.log(namen[i]);
+}
