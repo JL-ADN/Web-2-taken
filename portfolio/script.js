183ab136c2b9779c24a9d91d999e9ee7e7513cc5
Adrien <adrienguimard8@gmail.com>
2022-03-15 15:38:35 +0100

    Canvasloop 15/03/22

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,8 +2,4 @@ function myFunction() {
     var x = document.getElementById("snackbar");
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-  }
-  
-
-
- 
+}
